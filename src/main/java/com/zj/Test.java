b53aca324a0ca5764bf4b504266ccd31d79f89e2
zhaj <10015275+zhangj0108@user.noreply.gitee.com>
2021-11-15 10:02:29 +0800

    11.15

--- src/main/java/com/zj/Test.java
+++ src/main/java/com/zj/Test.java
@@ -4,5 +4,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("第1次提交");
         System.out.println("123");
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
     }
 }
