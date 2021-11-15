9815b4069b155c957d1761b60451ac0cf05cecac
zhaj <10015275+zhangj0108@user.noreply.gitee.com>
2021-11-15 09:59:25 +0800

    第2次提交

--- src/main/java/com/zj/Test.java
+++ src/main/java/com/zj/Test.java
@@ -3,8 +3,6 @@ package com.zj;
 public class Test {
     public static void main(String[] args) {
         System.out.println("第1次提交");
-        System.out.println("第2次提交");
-        System.out.println("第3次提交");
-        System.out.println("第4次提交");
+        System.out.println("123");
     }
 }
